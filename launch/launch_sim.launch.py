7fac325846535e1a3dba41454c441d0ea482ad11
premmm <prem.pannavit@gmail.com>
2026-05-18 15:44:08 +0700

    add ros2_control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,55 +1,43 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='my_bot'
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Find the exact filepath to your custom world
     world_path = os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacles.world')
 
-    # Include the Gazebo launch file, explicitly passing in the world_path
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-        launch_arguments={'gz_args': ['-r -v4', ' ',  world_path], 'on_exit_shutdown': 'true'}.items()
+        launch_arguments={'gz_args': ['-r -v4', ' ', world_path], 'on_exit_shutdown': 'true'}.items()
     )
 
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot',
-                                   '-z', '0.5'],
-                        output='screen')
-    
-    # Launch the ROS-Gazebo bridge for normal topics
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description',
+                   '-name', 'my_bot',
+                   '-z', '0.5'],
+        output='screen'
+    )
+
     bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
-        parameters=[{
-                'config_file': bridge_params,
-            }],
-            output='screen'
+        parameters=[{'config_file': bridge_params}],
+        output='screen'
     )
 
     ros_gz_image_bridge = Node(
@@ -58,12 +46,31 @@ def generate_launch_description():
         arguments=["/camera/image_raw"]
     )
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+        output='screen'
+    )
 
-    # Launch them all!
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+        output='screen'
+    )
+
+    # Wrap spawners in a TimerAction so they wait for Gazebo + controller_manager to be ready
+    delayed_spawners = TimerAction(
+        period=5.0,
+        actions=[diff_drive_spawner, joint_broad_spawner]
+    )
+
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
         ros_gz_bridge,
         ros_gz_image_bridge,
+        delayed_spawners,   
     ])
